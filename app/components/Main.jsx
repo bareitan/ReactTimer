f11b6aedbd78d0ef6992c96c55d1171f7299cc45
Bar Eitan <bareitan@microsoft.com>
2017-02-11 16:04:14 +0200

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = (props) => {
+    return (
+      <div>
+        <Nav/>
+      </div>
+    );
+  };
+module.exports = Main;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var {Link,IndexLink} = require('react-router')
+
+var Nav = React.createClass({
+  render:function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Time App
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active-link">CountDown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="https://github.com/bareitan/" target="_blank">Bar Eitan</a>
+            </li>
+          </ul>
+      </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Nav;
